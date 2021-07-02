53b7053ed429f3d2fd682950aa81a56e13767f21
Aji Santoso <17200019@gmail.com>
2021-07-02 14:06:53 +0700

    second commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -12,3 +12,14 @@ btn.on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: 0 }, '300');
 });
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
